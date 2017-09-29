1557ca365e47444df300ae9cb23309cdfc24b284
igxtraining <training@igx.com>
2017-09-29 12:58:59 -0400

    Initial Commit

---
+++ Console/Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            for(int i=1; i<11; 1++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
